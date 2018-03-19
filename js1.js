6341829de5a0e7bc6c5711015018c2cf785a8868
Deony <nostopmicky@gmail.com>
2018-03-19 17:35:43 +0200

    done

--- js1.js
+++ js1.js
@@ -29,34 +29,34 @@
 
 
 //Find the longest word in a string
-		function findLongestWord(str){
-			var newStr=str.split(' ',lengthNewStr),
-				lengthNewStr=newStr.length,
-				i,
-				maxStr;
+// 		function findLongestWord(str){
+// 			var newStr=str.split(' ',lengthNewStr),
+// 				lengthNewStr=newStr.length,
+// 				i,
+// 				maxStr;
 				
-			if(newStr[0].length>newStr[1].length){
-				maxStr=newStr[0].length;
-			}
-			else{
-				maxStr=newStr[1].length;
-			}
+// 			if(newStr[0].length>newStr[1].length){
+// 				maxStr=newStr[0].length;
+// 			}
+// 			else{
+// 				maxStr=newStr[1].length;
+// 			}
 
-			for(i=2;i<lengthNewStr;i+=1){
-				if(maxStr>=newStr[i].length){
-					//maxStr;
-				}
-				else{
-					maxStr=newStr[i].length;
-				}
-			}
-			return maxStr;
-		}
+// 			for(i=2;i<lengthNewStr;i+=1){
+// 				if(maxStr>=newStr[i].length){
+// 					//maxStr;
+// 				}
+// 				else{
+// 					maxStr=newStr[i].length;
+// 				}
+// 			}
+// 			return maxStr;
+// 		}
 
-		console.log(findLongestWord("What if we try a super-long word such as otorhinolaryngology"));
+// 		console.log(findLongestWord("What if we try a super-long word such as otorhinolaryngology"));
 
 
-//Title case a sentence
+// //Title case a sentence
 		function titleCase(str){
 			str=str.toLowerCase();
 			var newStr2=str.split(' ',lengthNewStr2),
@@ -71,3 +71,37 @@
 		}
 
 		console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
+
+
+
+//Mean value
+		function mean(arr){
+			var sum=0, i;
+
+			for(i=0;i<arr.length;i+=1){
+				sum+=arr[i];
+			}
+
+			return sum/arr.length;
+		}
+
+		console.log(mean([1, 4, 5, 9, 7, 7]))
+
+
+//Positive numbers. Square root.
+		function positive(arr){
+			var i, b=0;
+			for(i=0;i<arr.length-b;i+=1){
+				if(arr[i]>0){
+					arr[i]=Math.sqrt(arr[i]);
+				}
+				else{
+					arr.splice(i,1);
+				}
+			}
+
+			return arr;
+
+		}
+
+		console.log(positive([9, -3, 4, -9, 100]))
