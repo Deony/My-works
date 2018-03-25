8ebc6f69419f01db923acbb5133d5046aa3710a4
Deony <nostopmicky@gmail.com>
2018-03-26 00:25:01 +0300

    5_hwDone

--- js1.js
+++ js1.js
@@ -1,46 +1,46 @@
 //Repeat a string repeat a string
-		// function repeatStringNumTimes(str, num) {
-		//   var i, newStr='';
-		//   for (i=1; i<=num; i+=1) {
-		//     newStr+= str;        
-		//   }
+		function repeatStringNumTimes(str, num) {
+		  var i, newStr='';
+		  for (i=1; i<=num; i+=1) {
+		    newStr+= str;        
+		  }
 
-		//   return newStr;
-		// }
+		  return newStr;
+		}
 
-		// console.log(repeatStringNumTimes("*", 3));
+		console.log(repeatStringNumTimes("*", 3));
 
 // //or
-// 		function repeatStringNumTimes(str, num) {  
-//  		 return (num>0) ? str.repeat(num) : str='';
-// 		}
+		function repeatStringNumTimes(str, num) {  
+ 		 return (num>0) ? str.repeat(num) : str='';
+		}
 
-// 		console.log(repeatStringNumTimes("abc", -2));
+		console.log(repeatStringNumTimes("abc", -2));
 
 
 
 //Truncate a string
-		// function truncateString(str, num) {
-		//   var newStr,
-		//       dot = '...',
-		//       sizeStr = str.length-1,
-		//       sizeDot = dot.length;
+		function truncateString(str, num) {
+		  var newStr,
+		      dot = '...',
+		      sizeStr = str.length-1,
+		      sizeDot = dot.length;
 		  
-		//   if ( num <= 3 ) {    
-		//     newStr = str.slice(0, num) + dot;
-		//   }
-		//   else if ( sizeStr > num ) {
-		//     newStr = str.slice(0, num - sizeDot) + dot;
-		//   }
+		  if ( num <= 3 ) {    
+		    newStr = str.slice(0, num) + dot;
+		  }
+		  else if ( sizeStr > num ) {
+		    newStr = str.slice(0, num - sizeDot) + dot;
+		  }
 		  
-		//   else {
-		//     newStr = str.slice(0, num);    
-		//   }
+		  else {
+		    newStr = str.slice(0, num);    
+		  }
 		    
-		//   return newStr;
-		// }
+		  return newStr;
+		}
 
-		// console.log(truncateString("Absolutely Longer", 2));
+		console.log(truncateString("Absolutely Longer", 2));
 
 
 
