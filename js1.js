e6f9908fee2555b8be374dbc38e7450227320ce5
Deony <nostopmicky@gmail.com>
2018-03-27 19:23:12 +0300

    5_hwDone

--- js1.js
+++ js1.js
@@ -1,82 +1,42 @@
-//Repeat a string repeat a string
-		function repeatStringNumTimes(str, num) {
-		  var i, newStr='';
-		  for (i=1; i<=num; i+=1) {
-		    newStr+= str;        
-		  }
+//Chunky Monkey
+function chunkArrayInGroups(arr, size) {
+  
+  var k=0, i, j, newArr=[[]],
+      arrSize = arr.length,
+      row = Math.ceil(arr.length/size);
 
-		  return newStr;
+	if(k < arrSize) {
+
+	    for (i = 0; i < row; i+=1){
+	    	newArr[i]=[];
+
+			for (j = 0; j < size; j+=1) {
+				if(arr[k] == null){
+					break;
+				}
+				console.log(i,j);
+				newArr[i][j]=arr[k];
+				++k;
+			}			
+
 		}
-
-		console.log(repeatStringNumTimes("*", 3));
-
-// //or
-		function repeatStringNumTimes(str, num) {  
- 		 return (num>0) ? str.repeat(num) : str='';
-		}
-
-		console.log(repeatStringNumTimes("abc", -2));
-
-
-
-//Truncate a string
-		function truncateString(str, num) {
-		  var newStr,
-		      dot = '...',
-		      sizeStr = str.length-1,
-		      sizeDot = dot.length;
-		  
-		  if ( num <= 3 ) {    
-		    newStr = str.slice(0, num) + dot;
-		  }
-		  else if ( sizeStr > num ) {
-		    newStr = str.slice(0, num - sizeDot) + dot;
-		  }
-		  
-		  else {
-		    newStr = str.slice(0, num);    
-		  }
-		    
-		  return newStr;
-		}
-
-		console.log(truncateString("Absolutely Longer", 2));
-
-
-
-//task 1
-	function createEl(){
-		var arr = [1, 2, 3, 4, 5, 6],
-			myUl = document.createElement('ul'),
-			myLi,
-			i,
-			size=arr.length;
-
-			for ( i = 0; i < size; i+=1 ) {
-				myLi = document.createElement('li');	
-				myUl.appendChild(myLi);
-				myLi.innerHTML = arr[i];
-			}
-
-			document.body.appendChild(myUl);
 	}
+  
+
+  return newArr;
+}
+
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
 
 
-//task 2
-	function clone(){
-		var input=document.getElementById('INPUT');
-		var cl=input.cloneNode();
-		document.body.appendChild(cl);
-	}
 
-//task 3
-	function checkingPositiveNum(arr){
 
-		function pos(arr) {
-			return arr < 0;
-		}
+//Slasher Flick
+function slasher(arr, howMany) {
+  var newArr = arr.slice(howMany);
+  
 
-		return arr.some(pos);		
-	}
+  return newArr;
+}
 
-	console.log(checkingPositiveNum([1, -2, 3, 2, -4, 5]));
+slasher(["burgers", "fries", "shake"], 1);
